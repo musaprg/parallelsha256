b5f984aa0dcc02d3df9e725c3e90a93f4de1ecb2
Kotaro Inoue <k.musaino@gmail.com>
2020-04-19 01:29:25 +0900

    Fix to read a file and print its text

--- main.go
+++ main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
-    "fmt"
-    "crypto/sha256"
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	//"crypto/sha256"
 )
 
 func main() {
-    h := sha256.New()
-    h.Write([]byte("hello, world\n"))
-    fmt.Printf("%x", h.Sum(nil))
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: parallelsha256 <path_to_file>")
+		os.Exit(1)
+	}
+
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	r := bufio.NewReader(f)
+
+	for {
+		if l, err := r.ReadString('\n'); err == io.EOF {
+            break
+		} else if err != nil {
+			panic(err)
+        } else {
+			fmt.Print(l)
+			//h := sha256.New()
+			//h.Write([]byte("hello, world\n"))
+			//fmt.Printf("%x", h.Sum(nil))
+		}
+	}
+
 }
