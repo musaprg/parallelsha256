b98660aee3ccf4f0ddab81637b0ff9dea7bba936
Kotaro Inoue <k.musaino@gmail.com>
2020-06-04 14:41:55 +0900

    Fix to run tasks on go routines
    Signed-off-by: Kotaro Inoue <k.musaino@gmail.com>

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"bufio"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"os"
-	//"crypto/sha256"
+	"sync"
 )
+
+type Result struct {
+	order int
+	value string
+}
 
 func main() {
 	if len(os.Args) != 2 {
@@ -19,20 +26,43 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+    defer f.Close()
 
 	r := bufio.NewReader(f)
 
+	lines := []string{}
+
 	for {
 		if l, err := r.ReadString('\n'); err == io.EOF {
-            break
+			break
 		} else if err != nil {
 			panic(err)
-        } else {
-			fmt.Print(l)
-			//h := sha256.New()
-			//h.Write([]byte("hello, world\n"))
-			//fmt.Printf("%x", h.Sum(nil))
+		} else {
+			lines = append(lines, l)
 		}
 	}
 
+	c := make(chan Result, len(lines))
+	wg := &sync.WaitGroup{}
+
+	for i, l := range lines {
+		wg.Add(1)
+		go func(i int, s string) {
+			defer wg.Done()
+			h := sha256.Sum256([]byte(s))
+			c <- Result{order: i, value: hex.EncodeToString(h[:])}
+		}(i, l)
+	}
+	wg.Wait()
+
+	results := make([]string, len(lines), len(lines))
+
+    for i := 0; i < len(lines); i++ {
+		r := <-c
+		results[r.order] = r.value
+	}
+
+	for _,s := range results {
+		fmt.Println(s)
+	}
 }
